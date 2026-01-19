00f44066f9f492255229c303ed866a1fab03c60d
Teddy <mblitmanage@gmail.com>
2026-01-19 14:58:04 +0300

    fix controller name TaskTimeLogController

--- Modules/ProjectsTeams/Presentation/Routes/api.php
+++ Modules/ProjectsTeams/Presentation/Routes/api.php
@@ -1,44 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\ProjectsTeams\Presentation\Controllers\ProjectsteamsController;
+use Modules\ProjectsTeams\Presentation\Controllers\ProjectsTeamsController;
 
 Route::prefix('projectsTeams')->group(function () {
     // Public routes
-    Route::get('/', [ProjectsteamsController::class, 'index']);
-    Route::get('/search', [ProjectsteamsController::class, 'search']);
-    Route::get('/upcoming', [ProjectsteamsController::class, 'getUpcoming']);
-    Route::get('/statistics', [ProjectsteamsController::class, 'statistics']);
-    Route::get('/health', [ProjectsteamsController::class, 'health']);
-    Route::get('/status/{status}', [ProjectsteamsController::class, 'getByStatus']);
-    Route::get('/team/{teamId}', [ProjectsteamsController::class, 'getByTeam']);
+    Route::get('/', [ProjectsTeamsController::class, 'index']);
+    Route::get('/search', [ProjectsTeamsController::class, 'search']);
+    Route::get('/upcoming', [ProjectsTeamsController::class, 'getUpcoming']);
+    Route::get('/statistics', [ProjectsTeamsController::class, 'statistics']);
+    Route::get('/health', [ProjectsTeamsController::class, 'health']);
+    Route::get('/status/{status}', [ProjectsTeamsController::class, 'getByStatus']);
+    Route::get('/team/{teamId}', [ProjectsTeamsController::class, 'getByTeam']);
 
     // Protected routes (with authentication)
     Route::middleware(['auth:api'])->group(function () {
         // CRUD operations
-        Route::post('/', [ProjectsteamsController::class, 'store']);
-        Route::get('/{id}', [ProjectsteamsController::class, 'show']);
-        Route::put('/{id}', [ProjectsteamsController::class, 'update']);
-        Route::delete('/{id}', [ProjectsteamsController::class, 'destroy']);
+        Route::post('/', [ProjectsTeamsController::class, 'store']);
+        Route::get('/{id}', [ProjectsTeamsController::class, 'show']);
+        Route::put('/{id}', [ProjectsTeamsController::class, 'update']);
+        Route::delete('/{id}', [ProjectsTeamsController::class, 'destroy']);
 
         // Status management
-        Route::put('/{id}/status', [ProjectsteamsController::class, 'updateStatus']);
-        Route::put('/{id}/complete', [ProjectsteamsController::class, 'complete']);
-        Route::put('/{id}/on-hold', [ProjectsteamsController::class, 'putOnHold']);
-        Route::put('/{id}/cancel', [ProjectsteamsController::class, 'cancel']);
-        Route::put('/{id}/reactivate', [ProjectsteamsController::class, 'reactivate']);
+        Route::put('/{id}/status', [ProjectsTeamsController::class, 'updateStatus']);
+        Route::put('/{id}/complete', [ProjectsTeamsController::class, 'complete']);
+        Route::put('/{id}/on-hold', [ProjectsTeamsController::class, 'putOnHold']);
+        Route::put('/{id}/cancel', [ProjectsTeamsController::class, 'cancel']);
+        Route::put('/{id}/reactivate', [ProjectsTeamsController::class, 'reactivate']);
 
         // Restore soft-deleted project
-        Route::put('/{id}/restore', [ProjectsteamsController::class, 'restore']);
-        Route::get('/{id}/team-users', [ProjectsteamsController::class, 'getProjectTeamUsers'])
+        Route::put('/{id}/restore', [ProjectsTeamsController::class, 'restore']);
+        Route::get('/{id}/team-users', [ProjectsTeamsController::class, 'getProjectTeamUsers'])
             ->where('id', '[0-9]+');
 
-        Route::get('/{id}/assignable-users', [ProjectsteamsController::class, 'getAssignableUsers'])
+        Route::get('/{id}/assignable-users', [ProjectsTeamsController::class, 'getAssignableUsers'])
             ->where('id', '[0-9]+');
 
-        Route::get('/{id}/with-team', [ProjectsteamsController::class, 'showWithTeam'])
+        Route::get('/{id}/with-team', [ProjectsTeamsController::class, 'showWithTeam'])
             ->where('id', '[0-9]+');
         // Membership check
-        Route::get('/{projectId}/check-team/{teamId}', [ProjectsteamsController::class, 'checkTeamMembership']);
+        Route::get('/{projectId}/check-team/{teamId}', [ProjectsTeamsController::class, 'checkTeamMembership']);
     });
 });
